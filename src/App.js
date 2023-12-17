ee2d1faf2eaeebaf2c8b3d5e22dbf665f6765e94
jeannieyeliu <mmliuye@gmail.com>
2023-12-17 19:43:10 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          上海镇西国际物流业务
+          上海镇西国际物流有限公司
         </p>
         <a
           className="App-link"
